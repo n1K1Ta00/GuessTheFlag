afa3797fd7e1304062ace966b5c94fb668e6b69b
n1K1Ta00 <aleks.avers12@mail.ru>
2023-08-03 13:21:32 +0300

    add animation

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -14,6 +14,8 @@ struct ContentView: View {
                      "Ireland","Italy","Nigeria","Poland", "Russia","Spain","UK","US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
     @State private var score = 0
+    @State private var flag = false
+    @State private var animation = 0.0
 
     
     var body: some View {
@@ -31,14 +33,24 @@ struct ContentView: View {
                         .foregroundColor(.white)
                 }
                 ForEach(0..<3) {number in
+                    let isCorrectAnswer = number == correctAnswer
                     Button {
                         flagTapped(number)
-                        
+                        if isCorrectAnswer {
+                            withAnimation(.linear(duration: 1)) {
+                                animation += 360
+                            }
+                        }
                     } label: {
                         Image(countries[number])
                             .renderingMode(.original)
+                            .rotation3DEffect(.degrees(isCorrectAnswer ? animation : 0), axis: (x: 0, y: 1 , z: 0))
+                        
                     }
                 }
+
+
+
             }
         
         }
@@ -52,20 +64,33 @@ struct ContentView: View {
     func flagTapped(_ number:Int ){
         if number == correctAnswer{
             score = score+1
-        if score < 0 {
-            score = 0
+            if score < 0 {
+                score = 0
             }
             scoreTittle="Правильно"
         }
         else {
             score = score-1
             scoreTittle = "Неправильно"
-        if score < 0 {
+            if score < 0 {
                 score = 0
-                }
+            }
+            // уменьшаем масштаб и делаем флаг менее ярким для невыбранного флага
+            withAnimation(.easeOut) {
+                Image(countries[correctAnswer])
+                    .renderingMode(.original)
+                    .scaleEffect(0.5)
+                    .opacity(0.5)
+            
+        
+        showingScore = true
+    }
+
+
             
         }
         showingScore = true
+
     }
     func askQustion() {
         countries.shuffle()
